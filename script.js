50afdf06c9da9e066547640d12fe9e969619ef1e
Amanda Henrique <aamandahenrique@hotmail.com>
2019-04-19 17:47:50 -0300

    Animação menu com opções

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+let list = document.querySelector("#list");
 let menu = document.querySelector(".menu");
 let trace = document.querySelectorAll(".trace");
+
+list.classList.add("showList");
 
 click = () => {
     trace[0].classList.toggle("first");
     trace[1].classList.toggle("second");
     trace[2].classList.toggle("third");
     menu.classList.toggle("menuClicked");
+    list.classList.toggle("showList");
+
 }
 
 menu.onclick = click;
